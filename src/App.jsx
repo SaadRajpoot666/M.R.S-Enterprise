555f1f90eb44b88dd1110c621772d30812d71784
Saad Bin Khalid <khalidmehmoodeditz@gmail.com>
2025-05-26 23:37:56 +0500

    Changes in Navbar, App.jsx and HeroSection Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import { HeroSection } from "./components/HeroSection/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
 
 export default function App() {
+   useEffect(() => {
+     AOS.init({ duration: 1000, once: true }); // Once = animation only happens once
+   }, []);
    return <>
    <Navbar   />
    <HeroSection   />
